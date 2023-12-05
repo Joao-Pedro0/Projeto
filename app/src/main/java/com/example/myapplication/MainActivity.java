54c64a76192de0f2db6409bdf1e87d4767eb62fb
jpnor <jpnor@JOAOPEDRO>
2023-12-05 12:01:03 -0300

    Versão N2.2

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
@@ -11,11 +13,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 import java.util.List;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private EditText EditTextEmail;
     private EditText EditTextSenha;
     private TextView TextCadastro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,10 +57,8 @@ public class MainActivity extends AppCompatActivity {
                 u = dao.BuscarPorEmail(email);
                 Senha = u.getSenha();
                 break;
-
             }
         }
-
 
         if(validaEmail == false ){
             Toast.makeText(getApplicationContext(), "Email não cadastrado", Toast.LENGTH_SHORT).show();
